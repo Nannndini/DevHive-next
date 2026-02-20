2b116118d4d0a6cca276858dae3dfca9ea51d197
nandininaidu <143202941+Nannndini@users.noreply.github.com>
2026-02-20 21:51:45 +0530

    add supabase postgres database connection

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "PASTE_YOUR_SUPABASE_CONNECTION_STRING_HERE"
+DATABASE_URL = "postgresql://postgres:[YOUR-PASSWORD]@db.smsfqwxfiswxttzvpxmw.supabase.co:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 
