87c6f35cecdb1db9d2226561907471926475a854
nandininaidu <143202941+Nannndini@users.noreply.github.com>
2026-02-20 21:54:03 +0530

    create basic fastapi app with db initialization

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,12 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from database import engine
+from models import Base
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# create tables automatically
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
-    return {"message": "DevHive Backend Running"}
+    return {"message": "DevHive backend running"}
