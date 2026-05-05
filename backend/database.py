f7b3dbfc1e4f74e526937c720ed8322192bdee32
nandininaidu <nandini2005729@gmail.com>
2026-05-05 09:55:39 +0530

    fix: refactor database.py to use dotenv and ap-south-1 connection pool

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:KailashNandin@db.smsfqwxfiswxttzvpxmw.supabase.co:5432/postgres"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+password = os.getenv("SUPABASE_PASSWORD")
+DATABASE_URL = f"postgresql://postgres.smsfqwxfiswxttzvpxmw:{password}@aws-0-ap-south-1.pooler.supabase.com:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 
